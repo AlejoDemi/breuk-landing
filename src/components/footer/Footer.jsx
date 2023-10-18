c788bdb72529cfd5d7b997b1db348dc04d74bc87
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2023-10-18 10:45:25 -0300

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css"
+
+export const Footer = () =>{
+    return(
+        <div className="footer">
+            <div className="logo"></div>
+            <div className="contact-info">
+                <div className="contact-title">
+                    Contactanos
+                </div>
+                <div className="contact-data">
+
+                </div>
+            </div>
+        </div>
+    )
+}
